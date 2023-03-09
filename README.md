f5dc40e67bc0ed3124c5c53de921b8b3d374a050
em-ni <manunicotra97@gmail.com>
2023-03-09 15:23:29 +1100

    added data collection scripts

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # surgical-copilot
+
+# Create environment
+1. Follow the official guide to install [tensorflow](https://www.tensorflow.org/install/pip)
+2. To test GPU open Anaconda Prompt as admin and type
+```
+conda activate tf
+python ./test/tf_GPU.py
+
+```
+3. Install opencv using [conda](https://anaconda.org/conda-forge/opencv)
+4. Test cv2 with your webcam
+```
+python ./test/cv2_cam.py
+``` 
---
+++ tests/test.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+
+""" # Read image and output it as matrix of pixels
+img = cv.imread('Resources/Photos/cat.jpg')
+#img = cv.imread('Resources/Photos/cat_large.jpg')
+
+# Display image
+cv.imshow('Cat', img)
+
+# Wait for any key to be pressed
+cv.waitKey(0) """
+
+# Read video
+# To use camera connected to computer, use 0 as argument (or 1,2,3...)
+#capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+# get camera in linux
+capture = cv.VideoCapture(0)
+
+# Read video frame by frame
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    # Wait for any key to be pressed
+    if cv.waitKey(1) & 0xFF==ord('d'): # if the key pressed is 'd'
+        break
+
+# Release video
+capture.release()
+cv.destroyAllWindows()
