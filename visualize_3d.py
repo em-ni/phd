8097c68132d9fb69b075cac5bfad49206a392b5c
Emanuele-n <manunicotra97@gmail.com>
2024-12-31 15:51:10 +1100

    visualize multiple files

--- visualize_3d.py
+++ visualize_3d.py
@@ -1,26 +1,28 @@
 import argparse
-import pyvista as pv # type: ignore
+import pyvista as pv  # type: ignore
+
 
 def main():
     # Define the command line arguments using argparse
     parser = argparse.ArgumentParser(description="Visualize 3D files.")
-    parser.add_argument("-i", "--input", required=True, help="Path to the input file.")
+    parser.add_argument(
+        "-i", "--input", required=True, nargs="+", help="Paths to the input files."
+    )
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Load the VTK file
-    mesh = pv.read(args.input)
-
     # Create a plotter object
     plotter = pv.Plotter()
 
-    # Add the mesh to the plotter
-    plotter.add_mesh(mesh)
+    # Load and add each mesh to the plotter
+    for input_file in args.input:
+        mesh = pv.read(input_file)
+        plotter.add_mesh(mesh)
 
     # Display the plot
     plotter.show()
 
+
 if __name__ == "__main__":
     main()
-
